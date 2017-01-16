882213fbf47fc0302ee0ca7dbaa8c82dc1c207c7
René Sackers <rene.sackers@live.com>
2017-01-16 01:14:17 +0100

    Add project files.

---
+++ SqlCompactExample/resources/sqlcompact/Server/Main.cs
@@ -0,0 +1,49 @@
+﻿using GTANetworkServer;
+using SqlCompactExample.resources.sqlcompact.Server.Models;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SqlCompactExample.resources.sqlcompact.Server
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            API.onResourceStart += OnResourceStart;
+            API.onPlayerConnected += OnPlayerConnected;
+        }
+
+        private void OnResourceStart()
+        {
+            var databaseFilePath = API.getResourceFolder() + "\\Database.sdf";
+            ContextFactory.DatabaseFilePath = databaseFilePath;
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<DefaultDbContext, MigrationConfiguration>());
+
+            var uniqueUsers = ContextFactory.Instance.UserProfiles.Count();
+            API.consoleOutput("Unique players: " + uniqueUsers);
+        }
+
+        private void OnPlayerConnected(Client player)
+        {
+            var userProfile = ContextFactory.Instance.UserProfiles.FirstOrDefault(up => up.SocialClubName == player.socialClubName);
+
+            if (userProfile == null)
+            {
+                API.consoleOutput("New player: " + player.socialClubName);
+
+                userProfile = new UserProfile { SocialClubName = player.socialClubName };
+                ContextFactory.Instance.UserProfiles.Add(userProfile);
+            }
+            else
+            {
+                API.consoleOutput($"Returning player: {player.socialClubName}. Last display name: {userProfile.LastDisplayName}, current display name: {player.name}");
+            }
+
+            userProfile.LastIp = player.address;
+            userProfile.LastDisplayName = player.name;
+
+            ContextFactory.Instance.SaveChanges();
+        }
+    }
+}
---
+++ SqlCompactExample/resources/sqlcompact/Server/Models/DefaultDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.SqlServerCompact;
+
+namespace SqlCompactExample.resources.sqlcompact.Server.Models
+{
+
+    [DbConfigurationType(typeof(DatabaseConfiguration))]
+    public class DefaultDbContext : DbContext
+    {
+        public DefaultDbContext(string connectionString) : base(connectionString)
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+    public class ContextFactory : IDbContextFactory<DefaultDbContext>
+    {
+        public static string DatabaseFilePath;
+
+        public static DefaultDbContext Instance
+        {
+            get
+            {
+                if (DatabaseFilePath == null) throw new InvalidOperationException("Tried to use database context before setting file path.\nMake sure you set the DB file path on the ContextFactory in the onResourceStart event, before using it!");
+
+                return new DefaultDbContext("Data Source=" + DatabaseFilePath);
+            }
+        }
+
+        public ContextFactory()
+        {
+        }
+
+        public DefaultDbContext Create()
+        {
+            return Instance;
+        }
+    }
+
+    internal sealed class MigrationConfiguration : DbMigrationsConfiguration<DefaultDbContext>
+    {
+        public MigrationConfiguration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+    }
+
+    internal sealed class DatabaseConfiguration : DbConfiguration
+    {
+        public DatabaseConfiguration()
+        {
+            SetProviderServices(SqlCeProviderServices.ProviderInvariantName, SqlCeProviderServices.Instance);
+            SetDefaultConnectionFactory(new SqlCeConnectionFactory(SqlCeProviderServices.ProviderInvariantName));
+        }
+    }
+}
